4c51d9599afc03798ca203f8fae38e46a365b762
vanessa pyne <vipyne@gmail.com>
2015-10-06 23:40:35 -0400

    baaaaaaasic

---
+++ ch1-10.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+#define ACCELERATION 32
+
+int main (void) {
+
+    int timeInSeconds;
+    int distance;
+
+    std::cout << "enter time (in seconds):\n";
+    std::cin >> timeInSeconds;
+
+    // Distance=1/2 * acceleration * time ^ 2
+    distance = .5 * ACCELERATION * pow(timeInSeconds, 2.0);
+
+    std::cout << "object will fall a distance of " << distance << " feet.\n";
+
+    return 0;
+}
